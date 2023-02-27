27f6fdee1664bb4e0dc91bdd820534b26ad66e96
BrycensRanch <brycengranville@outlook.com>
2023-02-27 15:15:18 -0500

    build(fix): do not change version on ci
    Semantic Release handles channels for us more cleanly than local builds

--- update-versions.js
+++ update-versions.js
@@ -9,9 +9,10 @@ var files = fs.readdirSync("build/libs");
 
 files.forEach(function(file) {
     if (file.endsWith(".jar")) {
-        var fileHash = file.split("-")[1];
-        var newFileName = "build/libs/" + file.replace(fileHash, versionToSet);
-        if (!newFileName.includes("jar")) newFileName += ".jar";
-        fs.renameSync("build/libs/" + file, newFileName);
+        // var fileHash = file.split("-")[1];
+        // var newFileName = "build/libs/" + file.replace(fileHash, versionToSet);
+        // if (!newFileName.includes("jar")) newFileName += ".jar";
+        // fs.renameSync("build/libs/" + file, newFileName);
+        console.log("Not renaming " + file + "due to semantic policies");
     }
 });
