f3952bd9271a4a367fcebe107b0dee9960b716d5
Brycen G <brycengranville@outlook.com>
2023-03-10 10:29:02 +0000

    feat(ssl): add in-depth ssl options in config
    also added project.version to config.yml

---
+++ src/main/java/me/romvnly/TownyPlus/util/DatabaseType.java
@@ -0,0 +1,20 @@
+/*
+ * This file is part of TownyPlus, licensed under the GPL v3 License.
+ * Copyright (C) Romvnly <https://github.com/Romvnly-Gaming>
+ * Copyright (C) spigot-plugin-template team and contributors
+ * Copyright (C) Pl3xmap team and contributors
+ * Copyright (C) DiscordSRV team and contributors
+ * @author Romvnly
+ * @link https://github.com/Romvnly-Gaming/TownyPlus
+ */
+
+package me.romvnly.TownyPlus.util;
+
+// I can't believe I had to make a whole file to make this enum public. I love Java.
+
+public enum DatabaseType {
+    SQLITE,
+    MYSQL,
+    POSTGRESQL,
+    H2;
+}
